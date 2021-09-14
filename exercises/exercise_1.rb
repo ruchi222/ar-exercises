c1a5327b1b37e3616a17c56e07e35d33b63a1fbd
Ruchika Gosain <sharma.ruchika34@gmail.com>
2021-09-13 23:18:12 -0400

    Add the stores names

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+Store.create(name: "Burnaby",
+    annual_revenue: 300000,
+    mens apparel: true,
+    womaens apparel: true
+)
+
+Store.create(name: "Richmond",
+    annual_revenue: 1260000,
+    mens apparel: false,
+    womaens apparel: true
+)
+
+Store.create(name: "Gastown",
+    annual_revenue: 190000,
+    mens apparel: true,
+    womaens apparel: false
+)
+
+puts Store.count
